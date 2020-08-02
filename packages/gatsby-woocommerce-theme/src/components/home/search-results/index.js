50be9030d0c844220eb3f76b2f4c2e192aff8342
Imran Sayed <imranhsayed@gmail.com>
2020-08-02 17:25:26 +0530

    Add styles for posts

--- packages/gatsby-woocommerce-theme/src/components/home/search-results/index.js
+++ packages/gatsby-woocommerce-theme/src/components/home/search-results/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { isEmpty } from 'lodash';
 import Products from "../products";
+import './style.scss';
 
 const SearchResults = ( { queryResults, allProducts } ) => {
 
@@ -8,10 +9,11 @@ const SearchResults = ( { queryResults, allProducts } ) => {
 		return null;
 	}
 
+	// If search results are not available show all products.
 	const productsToBeDisplayed = ! isEmpty( queryResults ) ? queryResults : allProducts;
 
 	return (
-		<div className="search-resultss">
+		<div className="search-results">
 			<Products products={ productsToBeDisplayed }/>
 		</div>
 	);
