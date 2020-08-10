61d5fb5c81f0945d828ad371269a59e8a1839b36
Imran Sayed <imranhsayed@gmail.com>
2020-08-10 21:07:54 +0530

    Add @apollo/client, isomorphic-fetch, node-fetch

---
+++ packages/gatsby-woocommerce-theme/gatsby-ssr.js
@@ -0,0 +1,8 @@
+
+/**
+ * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/ssr-apis/
+ */
+
+export { wrapRootElement } from './src/apollo/wrap-root-element';
